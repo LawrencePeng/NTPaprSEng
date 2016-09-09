49736deece6edb32e6e9b59c16f2712ab7cb1a5a
Lawrence <lawrencepeng233@gmail.com>
2016-09-09 14:11:45 +0800

    refact

--- src/main/java/com/weapes/ntpaprseng/crawler/util/CreateSQL.java
+++ src/main/java/com/weapes/ntpaprseng/crawler/util/CreateSQL.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
  * Created by  不一样的天空  on 2016/8/30.
  */
 public class CreateSQL {
+
     // 第一次更新REF_DATA表的sql语句
     private static final String FIRST_REF_UPDATE_SQL =
             "UPDATE REF_DATA SET " + "Page_views = ?, " + "Web_of_Science = ?, " +
@@ -20,6 +21,7 @@ public class CreateSQL {
                     "Google = ?, " + "Pinterest = ?, " + "wikipedia = ?, " + "Mendeley = ?, " +
                     "CiteUlink = ?, " + "Zotero = ?, " + "F1000 = ?, " +
                     "Video = ?, " + "linkedin = ?, " + "Q_A = ? " + "WHERE URL = ";
+
     private static final String NT_PAPERS_INSERT_SQL =
             "INSERT INTO " + "NT_PAPERS(" +
                     "Title ," + "Authors, " + "SourceTitle, " + "ISSN, " +
@@ -31,16 +33,21 @@ public class CreateSQL {
             "INSERT INTO REF_DATA(URL, Page_views, Web_of_Science, CrossRef, Scopus, News_outlets, " +
                     "reddit, Blog, Tweets, Facebook, Google, Pinterest, wikipedia, Mendeley, CiteUlink, Zotero, F1000, Video, " +
                     "linkedin, Q_A)" + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
     private static final String columns[] = {"Page_views_", "Web_of_Science_",
             "CrossRef_", "Scopus_", "News_outlets_", "reddit_", "Blog_",
             "Tweets_", "Facebook_", "Google_", "Pinterest_", "wikipedia_",
             "Mendeley_", "CiteUlink_", "Zotero_", "F1000_", "Video_", "linkein_", "Q_A_"};
+
     private static String ADD_COLUMNS_SQL = "ALTER TABLE REF_DATA ADD(";//添加REF_DATA表里的字段的SQL
+
     private static String REF_UPDATE_SQL = "UPDATE REF_DATA SET ";//从第二次爬取开始更新REF_DATA表的SQL
     private static String DEFAULT_ATTRIBUTE = " INT(11) NULL DEFAULT 0,";//向REF_DATA表添加字段的默认属性
     private static String UPDATE_TIME_SQL = "SELECT time FROM TIME";
     private static String CHANGE_UPDATE_TIME_SQL = "INSERT INTO TIME(time,date)" + "VALUES(?, ?)";
+
     private static String TIME = getUpdateTime();//从数据库中获取的第几次爬取的值
+
     private CreateSQL() {
 
     }
@@ -119,7 +126,7 @@ public class CreateSQL {
         }
     }
 
-    public static boolean executeChangeUpdateTimeSQL() {
+    static boolean executeChangeUpdateTimeSQL() {
         try {
             final HikariDataSource mysqlDataSource = DataSource.getMysqlDataSource();
             final Connection connection = mysqlDataSource.getConnection();
